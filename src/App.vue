7a6c274a6c8783ddcf4bf8eae2ec5a4aa4e76c5c
迪天祥 <1426155410@qq.com>
2021-07-31 12:21:52 +0800

    事件总线完成

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <Search></Search>
+    <List></List>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  import List from './components/List.vue';
+  import Search from './components/Search.vue';
+  export default {
+    name: 'App',
+    components: {
+      List,
+      Search
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Search.vue
@@ -0,0 +1,53 @@
+<template>
+  <section class="jumbotron">
+    <h3 class="jumbotron-heading">Search Github Users</h3>
+    <div>
+      <input type="text" placeholder="enter the name you search" v-model="keyWord" />&nbsp;
+      <button @click="searchUsers">Search</button>
+    </div>
+  </section>
+</template>
+
+<script>
+  import axios from 'axios';
+  export default {
+    name: 'Search',
+    data() {
+      return {
+        keyWord: ''
+      }
+    },
+    methods: {
+      searchUsers() {
+        //请求前更新List数据
+        this.$bus.$emit('updateListData', {
+          isFirst: false,
+          isLoading: true,
+          errMsg: '',
+          users: []
+        })
+        axios.get(`https://api.github.com/search/users?q=${this.keyWord}`).then((res) => {
+          console.log('请求成功了', res.data);
+          //请求成功后更新List数据
+          this.$bus.$emit('updateListData', {
+            isLoading: false,
+            errMsg: '',
+            users: res.data.items
+          })
+        }).catch((err) => {
+          console.log('请求失败了', err.message);
+          //请求失败后更新List数据
+          this.$bus.$emit('updateListData', {
+            isLoading: false,
+            errMsg: err,
+            users: []
+          })
+        });
+      }
+    },
+  }
+</script>
+
+<style>
+
+</style>
